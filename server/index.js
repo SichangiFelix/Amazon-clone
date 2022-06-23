236dca331e741008c7db5d0bf17caf9b9c54bc87
SichangiFelix <sichangifelix23@gmail.com>
2022-06-23 20:08:07 +0300

    created the first app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 console.log("Hello, World");
 
-//
+
 const express = require('express');
+const PORT = 3000;
+
+const app = express();
 
 //Creating an API
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+});
+//localhost
+//
