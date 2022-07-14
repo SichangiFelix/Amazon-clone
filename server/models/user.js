733ffa9447efac1ce9a95a5261ce5df40ee2aa58
SichangiFelix <sichangifelix23@gmail.com>
2022-07-15 00:19:15 +0300

    Updates

--- server/models/user.js
+++ server/models/user.js
@@ -12,8 +12,27 @@ const userSchema = mongoose.Schema({
         trim: true,
         validate: {
             validator: (value) => {
-                
-            }
+                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                return value.match(re);
+            },
+            message : "Please enter a valid email address",
         }
-    }
+    },
+    password: {
+        required: true,
+        type: String,
+    },
+    address: {
+        type: String,
+        default: '',
+    },
+    type: {
+        type: String,
+        default: 'user',
+    },
+    //cart
 });
+
+const user = mongoose.model('User', userSchema);
+
+module.exports = User;
