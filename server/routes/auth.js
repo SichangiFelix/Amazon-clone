7624006ea718c172b4f54cf8e0d0d7f8bda0f183
SichangiFelix <sichangifelix23@gmail.com>
2022-07-26 09:26:02 +0300

    completed the signin route in auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
-
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 //singup
 authRouter.post("/api/signup", async (req, res) => {
@@ -30,5 +30,28 @@ authRouter.post("/api/signup", async (req, res) => {
     }
 
 });
+
+//sign in
+authRouter.post("/api/signin", async (re, res) => {
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg: "User with this email does not exsist!"});
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg: "Incorrect password."});
+        }
+        const token = jwt.sign({id: user._id}, "passwordKey");
+        re.json({token, ...user._doc});
+
+    }catch (e){
+        res.status(500).json({error: e.message});
+    }
+});
+
 
 module.exports = authRouter; //makes it accessible in other files
