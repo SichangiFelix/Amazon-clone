1c5a2d64ce752535e239c592fd1f97378f623551
SichangiFelix <sichangifelix23@gmail.com>
2022-07-26 23:24:27 +0300

    shared prefs and change notifier

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/utils.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../constants/global_variables.dart';
 import '../../../models/user.dart';
@@ -38,6 +41,39 @@ class AuthService {
         onSuccess: () {
           showSnackBar(
               context, 'Account created! Login with the same credentials');
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  //sign in the user
+  void signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signin'),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      print(res.body);
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
         },
       );
     } catch (e) {
