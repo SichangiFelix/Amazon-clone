f3e916927bc8ddafb1ebd4fb82af50628ba34cbc
SichangiFelix <sichangifelix23@gmail.com>
2022-09-20 08:30:49 +0300

    Click event for openning category screen

--- lib/features/home/widgets/top_categories.dart
+++ lib/features/home/widgets/top_categories.dart
@@ -1,8 +1,13 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/home/screens/category_deals_screen.dart';
 import 'package:flutter/material.dart';
 
 class TopCategories extends StatelessWidget {
   const TopCategories({Key? key}) : super(key: key);
+
+  void navigateToCategories(BuildContext context, String category){
+    Navigator.pushNamed(context, CategoryDealsScreen.routeName, arguments: category);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,28 +19,33 @@ class TopCategories extends StatelessWidget {
         itemExtent: 75,
         itemCount: GlobalVariables.categoryImages.length,
         itemBuilder: (context, index){
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.asset(
-                    fit: BoxFit.cover,
-                    height: 40,
-                    width: 40,
-                    GlobalVariables.categoryImages[index]['image']!,
+          return GestureDetector(
+            onTap: (){
+                navigateToCategories(context, GlobalVariables.categoryImages[index]['title']!);
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.asset(
+                      fit: BoxFit.cover,
+                      height: 40,
+                      width: 40,
+                      GlobalVariables.categoryImages[index]['image']!,
+                    ),
                   ),
                 ),
-              ),
-              Text(GlobalVariables.categoryImages[index]['title']!,
-                style: const TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
+                Text(GlobalVariables.categoryImages[index]['title']!,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         }),
     );
