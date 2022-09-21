dca80651307b57e970b0e1eb36ada621997634f7
SichangiFelix <sichangifelix23@gmail.com>
2022-09-21 16:15:06 +0300

    Finalised rating functionality

--- lib/features/search/widgets/searched_product.dart
+++ lib/features/search/widgets/searched_product.dart
@@ -9,6 +9,16 @@ class SearchedProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    double totalRating = 0;
+    for(int i = 0; i < product.rating!.length; i++){
+      totalRating += product.rating![i].rating;
+    }
+double avgRating = 0;
+    if(totalRating != 0){
+      avgRating = totalRating / product.rating!.length;
+    }
+
     return Column(
       children: [
         Container(
@@ -38,7 +48,7 @@ class SearchedProduct extends StatelessWidget {
                 Container(
                   width: 235,
                   padding: const EdgeInsets.only(left: 10, top: 5),
-                  child: const Stars(rating: 3),
+                  child: Stars(rating: avgRating),
                 ),
                 Container(
                   width: 235,
