2532a0362da8bda9bb3d7488160c0417a158b031
SichangiFelix <sichangifelix23@gmail.com>
2022-09-21 15:10:39 +0300

    Completed rating

--- lib/features/product_details/screens/product_details_screen.dart
+++ lib/features/product_details/screens/product_details_screen.dart
@@ -5,10 +5,12 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:provider/provider.dart';
 
 import '../../../common/widgets/stars.dart';
 import '../../../constants/global_variables.dart';
 import '../../../models/product.dart';
+import '../../../provider/user_provider.dart';
 import '../../search/screens/search_screen.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
@@ -27,6 +29,25 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
 
   void navigateToSearchScreen(String query) {
     Navigator.pushNamed(context, SearchScreen.routeName, arguments: query);
+  }
+
+double avgRating = 0;
+double myRating = 0;
+
+  @override
+  void initState() {
+    double totalRating = 0;
+    for(int i = 0; i < widget.product.rating!.length; i++){
+      totalRating += widget.product.rating![i].rating;
+      if(widget.product.rating![i].userId == Provider.of<UserProvider>(context, listen: false).user.id){
+        myRating = widget.product.rating![i].rating;
+      }
+    }
+
+    if(totalRating != 0){
+      avgRating = totalRating / widget.product.rating!.length;
+    }
+    super.initState();
   }
 
   @override
@@ -105,7 +126,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(widget.product.id!),
-                  const Stars(rating: 4),
+                  Stars(rating: avgRating),
                 ],
               ),
             ),
@@ -184,7 +205,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
               ),),
             ),
             RatingBar.builder(
-              initialRating: 0,
+              initialRating: myRating,
               minRating: 1,
               direction: Axis.horizontal,
               allowHalfRating: true,
