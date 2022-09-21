f4af42fc84640c370c3c52fecad6a75eff2e86fb
SichangiFelix <sichangifelix23@gmail.com>
2022-09-21 09:15:42 +0300

    Product details completed UI and its services

--- lib/features/product_details/screens/product_details_screen.dart
+++ lib/features/product_details/screens/product_details_screen.dart
@@ -1,18 +1,21 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:amazon_clone/common/widgets/custom_button.dart';
+import 'package:amazon_clone/features/product_details/services/product_details_services.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-import 'package:flutter/src/foundation/key.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
+import '../../../common/widgets/stars.dart';
 import '../../../constants/global_variables.dart';
 import '../../../models/product.dart';
 import '../../search/screens/search_screen.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
-
   static const String routeName = '/product-details';
   final Product product;
-  const ProductDetailsScreen({Key? key, required this.product}) : super(key: key);
+  const ProductDetailsScreen({Key? key, required this.product})
+      : super(key: key);
 
   @override
   State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
@@ -20,19 +23,20 @@ class ProductDetailsScreen extends StatefulWidget {
 
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
 
-  void navigateToSearchScreen(String query){
+  final productDetailsServices = ProductDetailsServices();
+
+  void navigateToSearchScreen(String query) {
     Navigator.pushNamed(context, SearchScreen.routeName, arguments: query);
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(60),
         child: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
+          title:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Expanded(
               child: Container(
                 height: 42,
@@ -43,27 +47,32 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                   child: TextFormField(
                     onFieldSubmitted: navigateToSearchScreen,
                     decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: const EdgeInsets.only(top: 10),
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.all(Radius.circular(7))),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.only(top: 10),
+                        border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(Radius.circular(7))),
                         enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black38, width: 1),
-                        borderRadius: BorderRadius.all(Radius.circular(7))),
+                            borderSide:
+                                BorderSide(color: Colors.black38, width: 1),
+                            borderRadius: BorderRadius.all(Radius.circular(7))),
                         hintText: 'Search Amazon.in',
                         hintStyle: const TextStyle(
                           fontWeight: FontWeight.w500,
-                          fontSize:17,
+                          fontSize: 17,
                         ),
-                      prefixIcon: InkWell(
-                        onTap: (){},
-                        child: const Padding(padding: EdgeInsets.only(left: 6),
-                            child: Icon(Icons.search, color: Colors.black, size: 23,),
-                        ),
-                      )
-                    ),
+                        prefixIcon: InkWell(
+                          onTap: () {},
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 6),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                              size: 23,
+                            ),
+                          ),
+                        )),
                   ),
                 ),
               ),
@@ -72,7 +81,11 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
               color: Colors.transparent,
               height: 42,
               margin: const EdgeInsets.symmetric(horizontal: 10),
-              child: const Icon(Icons.mic, color: Colors.black, size: 25,),
+              child: const Icon(
+                Icons.mic,
+                color: Colors.black,
+                size: 25,
+              ),
             ),
           ]),
           flexibleSpace: Container(
@@ -80,6 +93,110 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
               gradient: GlobalVariables.appBarGradient,
             ),
           ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.product.id!),
+                  const Stars(rating: 4),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              child: Text(
+                widget.product.name,
+                style: const TextStyle(fontSize: 15),
+              ),
+            ),
+            CarouselSlider(
+              items: widget.product.images.map((i) {
+                return Builder(
+                  builder: (BuildContext context) => Image.network(
+                    i,
+                    fit: BoxFit.contain,
+                    height: 200,
+                  ),
+                );
+              }).toList(),
+              options: CarouselOptions(
+                viewportFraction: 1,
+                height: 300,
+              ),
+            ),
+            const SizedBox(height: 8,),
+            Container(
+              color: Colors.black12,
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: RichText(
+                text: TextSpan(
+                    text: 'Deal Price:',
+                    style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                        text: ' \$${widget.product.price}',
+                        style: const TextStyle(
+                            fontSize: 22,
+                            color: Colors.red,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ]),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(8),
+              child: Text(widget.product.description),
+            ),
+            Container(
+              color: Colors.black12,
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: CustomButton(text: 'Buy Now', onTap: (){})),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+              padding: const EdgeInsets.all(10),
+              child: CustomButton(text: 'Add to Cart', onTap: (){}, color: const Color.fromRGBO(254, 216, 19, 1),)),
+              const SizedBox(height: 10,),
+              Container(
+              color: Colors.black12,
+              height: 5,
+            ),
+            const Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text('Rate The Product', style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+            RatingBar.builder(
+              initialRating: 0,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  return const Icon(Icons.star, color: GlobalVariables.secondaryColor,);
+                  },
+                onRatingUpdate: (rating) {
+                  productDetailsServices.rateProduct(context: context, product: widget.product, rating: rating);
+                },
+              ),
+          ],
         ),
       ),
     );
