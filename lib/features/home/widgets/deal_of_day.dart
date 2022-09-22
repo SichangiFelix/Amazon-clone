ed81c0a2aaf26cd010385b7df6a64369e52307c3
SichangiFelix <sichangifelix23@gmail.com>
2022-09-22 09:57:36 +0300

    Displaying deal of the day

--- lib/features/home/widgets/deal_of_day.dart
+++ lib/features/home/widgets/deal_of_day.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../common/widgets/loader.dart';
+import '../../../models/product.dart';
+import '../../product_details/screens/product_details_screen.dart';
+import '../services/home_services.dart';
+
 class DealOfDay extends StatefulWidget {
   const DealOfDay({Key? key}) : super(key: key);
 
@@ -8,51 +13,89 @@ class DealOfDay extends StatefulWidget {
 }
 
 class _DealOfDayState extends State<DealOfDay> {
+  Product? product;
+  final homeServices = HomeServices();
+
+  @override
+  void initState() {
+    fetchDealOfDay();
+    super.initState();
+  }
+
+  void fetchDealOfDay() async {
+    product = await homeServices.fetchDealOfDay(context: context);
+    setState(() {
+      
+    });
+  }
+
+  void navigateToDetailsScreen(){
+    Navigator.pushNamed(context, ProductDetailsScreen.routeName, arguments: product);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 10, top: 15),
-          child: const Text('Deal of the day',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-        Image.network('https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvbmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', height: 235, fit: BoxFit.fitHeight,),
-        Container(
-          padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-          alignment: Alignment.topLeft,
-          child: const Text('\$100', style: TextStyle(fontSize: 18),),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-            padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-            child: const Text('Prime', 
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+    return product == null
+        ? const Loader()
+        : product!.name.isEmpty
+        ? const SizedBox()
+        : GestureDetector(
+          onTap: navigateToDetailsScreen,
+          child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(left: 10, top: 15),
+                  child: const Text(
+                    'Deal of the day',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Image.network(
+                  product!.images[0],
+                  height: 235,
+                  fit: BoxFit.fitHeight,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    '\$100',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
+                  child: const Text(
+                    'Prime',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: product!.images.map((e) => Image.network(
+                        e,
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.fitWidth,
+                      ),).toList(),
+                    
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                  ).copyWith(left: 15),
+                  alignment: Alignment.topLeft,
+                  child: Text('See all deals',
+                      style: TextStyle(color: Colors.cyan[800])),
+                ),
+              ],
             ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network('https://media.istockphoto.com/photos/girl-showing-mobile-advertisement-mockup-area-and-celebrating-her-picture-id1368018367?b=1&k=20&m=1368018367&s=170667a&w=0&h=1NppWaMJ3ggEYNZco4tS5phf56hdC71kRIzOqRhb0zM=', height: 100, width: 100, fit: BoxFit.fitWidth,),
-              Image.network('https://media.istockphoto.com/photos/girl-showing-mobile-advertisement-mockup-area-and-celebrating-her-picture-id1368018367?b=1&k=20&m=1368018367&s=170667a&w=0&h=1NppWaMJ3ggEYNZco4tS5phf56hdC71kRIzOqRhb0zM=', height: 100,width: 100, fit: BoxFit.fitWidth,),
-              Image.network('https://media.istockphoto.com/photos/girl-showing-mobile-advertisement-mockup-area-and-celebrating-her-picture-id1368018367?b=1&k=20&m=1368018367&s=170667a&w=0&h=1NppWaMJ3ggEYNZco4tS5phf56hdC71kRIzOqRhb0zM=', height: 100,width: 100, fit: BoxFit.fitWidth,),
-              Image.network('https://media.istockphoto.com/photos/girl-showing-mobile-advertisement-mockup-area-and-celebrating-her-picture-id1368018367?b=1&k=20&m=1368018367&s=170667a&w=0&h=1NppWaMJ3ggEYNZco4tS5phf56hdC71kRIzOqRhb0zM=', height: 100,width: 100, fit: BoxFit.fitWidth,),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 15,
-          ).copyWith(left: 15),
-          alignment: Alignment.topLeft,
-          child: Text('See all deals', style: TextStyle(color: Colors.cyan[800])),
-        ),
-      ],
-    );
+        );
   }
 }
