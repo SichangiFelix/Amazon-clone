229e295a4dd543e3f0edc3288d8a04268d26a20c
SichangiFelix <sichangifelix23@gmail.com>
2022-10-03 16:31:09 +0300

    Completed the app

--- lib/features/account/services/account_services.dart
+++ lib/features/account/services/account_services.dart
@@ -1,37 +1,41 @@
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../constants/error_handling.dart';
 import '../../../constants/global_variables.dart';
 import '../../../constants/utils.dart';
 import '../../../models/order.dart';
 import '../../../provider/user_provider.dart';
+import '../../auth/screens/auth_screen.dart';
 
 class AccountServices {
-  Future<List<Order>> fetchMyOrders(
-      {required BuildContext context}) async {
+  Future<List<Order>> fetchMyOrders({
+    required BuildContext context,
+  }) async {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     List<Order> orderList = [];
     try {
-      http.Response res = await http.get(
-        Uri.parse('$uri/api/orders/me'),
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-          'x-auth-token': userProvider.user.token,
-        },
-      );
+      http.Response res =
+          await http.get(Uri.parse('$uri/api/orders/me'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
 
       httpErrorHandle(
         response: res,
         context: context,
         onSuccess: () {
           for (int i = 0; i < jsonDecode(res.body).length; i++) {
-            orderList.add(Order.fromJson(jsonEncode(
-              jsonDecode(res.body)[i],
-            )));
+            orderList.add(
+              Order.fromJson(
+                jsonEncode(
+                  jsonDecode(res.body)[i],
+                ),
+              ),
+            );
           }
         },
       );
@@ -39,5 +43,21 @@ class AccountServices {
       showSnackBar(context, e.toString());
     }
     return orderList;
+  }
+
+  void logOut(BuildContext context) async {
+    try {
+      SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+      await sharedPreferences.setString('x-auth-token', '');
+      // ignore: use_build_context_synchronously
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        AuthScreen.routeName,
+        (route) => false,
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
